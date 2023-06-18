97c2b98024aa66c5e1f4d0e8593118cb1009f0a0
AnnaParulava <ap554156@yandex.ru>
2023-06-18 17:56:23 +0300

    homefragment updated

--- app/src/main/java/com/example/basketball/ui/HomeFragment.kt
+++ app/src/main/java/com/example/basketball/ui/HomeFragment.kt
@@ -6,33 +6,54 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.basketball.api.ApiFactory.apiService
 import com.example.basketball.databinding.FragmentHomeBinding
 import com.example.basketball.ui.adapter.MatchRecyclerAdapter
+import com.example.basketball.viewmodel.MatchViewModel
 import kotlinx.coroutines.launch
 
 class HomeFragment : Fragment() {
 
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    private var _binding: FragmentHomeBinding? = null
 
-        lifecycleScope.launch {
+    private val binding get() = _binding
+        ?: throw RuntimeException("Should only use binding after onCreateView and before onDestroyView")
 
-            val responseCategory = apiService.getCountries()
-            Log.d("AAA", "Category: ${responseCategory}")
-        }
-
-        val adapter = MatchRecyclerAdapter()
-        binding.matchesRecyclerView.adapter = adapter
-        //// viewmodel
+    private val viewModel by lazy {
+        ViewModelProvider(this)[MatchViewModel::class.java]
     }
 
 
-    private var _binding: FragmentHomeBinding? = null
-    private val binding get() = _binding
-        ?: throw RuntimeException("Should only use binding after onCreateView and before onDestroyView")
+    private val matchAdapter by lazy {
+        MatchRecyclerAdapter()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initRecyclerView()
+        observeMatches()
+    }
+
+    private fun initRecyclerView() {
+        binding.matchesRecyclerView.apply {
+            adapter = matchAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+        }
+    }
+
+    private fun observeMatches() {
+        viewModel.matches.observe(viewLifecycleOwner, Observer { matches ->
+            matchAdapter.submitList(matches)
+            matchAdapter.matchList = matches
+        })
+    }
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
